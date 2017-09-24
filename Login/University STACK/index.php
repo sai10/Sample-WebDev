cc75b8d91b116394609cc4b5c55e8aaefb1c9356
sai10 <priyankitastro10@gmail.com>
2017-09-24 17:40:39 +0530

    updated

---
+++ Login/University STACK/index.php
@@ -0,0 +1,73 @@
+
+
+
+<!DOCTYPE html>
+<html lang>
+
+	<head>
+  		<script type="text/javascript" src="js/jquery-2.1.4.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/scripts.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+ 
+
+	<style style="text/css">
+		html {
+ 		  	margin: 0px;
+   			height: 100%;
+		    width: 100%;
+		}
+
+		body {
+   			margin: 0px;
+   			min-height: 100%;
+   			width: 100%;
+		}
+		.img{
+  			background-image: url("img/photo_splash_signin_1141x759_v4.jpg");
+			background-size: 100% 100%;
+    		background-repeat: no-repeat;
+    	}
+
+	</style>
+
+
+
+ 	<body  class="img">
+ 	
+    	<div class="container">
+			<div class="row">
+            		<div class="col-sm-2 col-md-3 col-lg-3">
+					</div>
+					
+					<div class="col-sm-6 col-md-4 col-lg-4" style="padding-left:55px;">
+                    <div style="margin-top:35%;">
+                        	<img src="img/com.kryptosmobile.IIITBHU-w250.png" width="100%" height="100px"/>
+                    </div>
+					<div class="jumbotron" style="width:375px;background:#3b5998;opacity:0.7;">
+						<div class="text-center">
+							<div class="form-group">
+								<div class="input-group">
+  									<span class="input-group-addon" id="basic-addon1" style="width:100%;">USER ID</span>
+									<input type="text" name="user_id" maxlength="6" size="6" class="form-control" placeholder="Username"  autocomplete="off" style="width:150px;">
+								</div>
+								<br>
+								<div class="input-group">
+  									<span class="input-group-addon" id="basic-addon1">PASSWORD</span>
+									<input type="password" name="password" class="form-control" placeholder="Password" >
+								</div>
+								<br>
+								<input type="submit" class="btn btn-primary pull-right" style="width:255px;" value="Log In" name="login">
+								<br/>
+							</div>
+						</div>
+					</div>
+					</div>
+            </div>
+ 		</div>
+	
+ 		
+    </body>
+ </html>
